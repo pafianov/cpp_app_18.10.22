549e5c2649c03dc9e93d60335bdeb5e6f70a9603
s.pafianov <s.pafianov@SERVICE>
2022-11-02 11:21:09 +0200

    Task # 1

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include<iostream>
-using namespace std;
+using namespace std; 
+
+// Завдання 1. Напишіть програму, яка обчислює суму цілих
+//чисел від а до 500 (значення a вводиться з клавіатури).
 
 int main()
 {
+	
+	int a, sum=0;
+	cout << "Enter a -> ";
+	cin >> a;
+	for (size_t i = a; i <= 500; i++)
+	{
+		sum += i;
+	}
+	cout << "RES = " << sum << endl;	
 
-
-
-
+		
 	return 0;
 }
