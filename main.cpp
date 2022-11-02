6d180aae02d5e47c0210ec26544b43379a690602
s.pafianov <s.pafianov@SERVICE>
2022-11-02 11:53:23 +0200

    Task # 3

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include<iostream>
 using namespace std;
 
+// Завдання 3. Знайти середнє арифметичне всіх цілих чисел 
+// від 1 до 1000.
+
 int main()
 {
-
-
-
-
+	int sum=0, res;
+	for (size_t i = 1; i <= 1000; i++)
+	{
+		sum += i;
+		res = sum / i;
+	}
+	cout << "Res-> " << res << endl;
 	return 0;
 }
