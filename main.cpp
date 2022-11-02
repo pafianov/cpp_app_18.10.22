4a90ff3dc38dbc16ed113714fc8f3a0c9cb2824f
s.pafianov <s.pafianov@SERVICE>
2022-11-02 11:58:23 +0200

    Task # 4

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include<iostream>
 using namespace std;
 
+// Завдання 4. Знайти здобуток усіх цілих чисел від a до 20 
+// (значення a вводиться з клавіатури : 1 <= a <= 20).
+
 int main()
 {
-
-
+	int a=1,sum=0;
+	cout << "Enter a ->";
+	cin >> a;
+	for (size_t i = a; i <= 20; i++)
+	{
+		sum += i;
+	}
+	cout << "Res = " << sum << endl;
 
 
 	return 0;
