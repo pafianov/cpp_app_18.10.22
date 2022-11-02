7355d1fd2bc6d243cdc926c4d79b6af8077f391f
s.pafianov <s.pafianov@SERVICE>
2022-11-02 12:04:51 +0200

    Task # 5

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
 using namespace std;
 
+// Завдання 5. Написати програму, яка виводить на екран 
+// таблицю множення на k, де k — номер варіанта.
+
 int main()
 {
-
+	int k, res;
+	cout << "Enter variant number (2-9) ->";
+	cin >> k;
+	for (size_t i = 2; i <= 9; i++)
+	{
+		res = k * i;
+		cout << k << " * " << i << " = " << res << endl;
+	}
 
 
 
