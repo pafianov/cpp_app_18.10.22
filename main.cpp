970b7c9542178b5314a9244bf97d6daf038bfe2d
s.pafianov <s.pafianov@SERVICE>
2022-11-02 11:44:43 +0200

    Task # 2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include<iostream>
 using namespace std;
 
+//  Завдання 2. Напишіть програму, яка запитує два цілих
+//  числа x і y, після чого обчислює і виводить значення x у
+//  степені y.
+
+
 int main()
 {
-
+	int base, exponent, res = base = 1;
+	cout << "Enter base ->";
+	cin >> base;
+	cout << "Enter exponent ->";
+	cin >> exponent;
+	for (size_t i = 1; i <= exponent; i++)
+	{
+		res = base * res;
+	}
+	cout << "Exponention -> " << res << endl;
 
 
 
