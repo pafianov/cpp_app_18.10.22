b35f96077fced3e56c456f04e8317255050509fc
s.pafianov <s.pafianov@SERVICE>
2022-11-02 12:46:46 +0200

    Task # 7

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include<iostream>
 using namespace std;
 
+// Завдання 2. Підрахувати кількість цілих чисел у діапазоні 
+// від 100 до 999, у яких усі цифри різні.
+
 int main()
 {
-
+	int a = 0;
+	int i1, i2, i3;
+	for (size_t i = 100; i <= 999; i++)
+	{
+		i1 = i / 100;
+		i2 = i % 10;
+		i3 = (i % 100) / 10;
+		if (i1 != i2&&i2 != i3&&i3 != i1)
+		{
+          a++;
+		}
+		
+	}
+	cout << "Result:" << a << endl;
 
 
 
